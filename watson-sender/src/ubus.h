c8cc03b4b1677d090cfeca8eb49b81325a61314a
Nojus Markevičius <31002073+nojusm02@users.noreply.github.com>
2023-01-20 11:09:18 +0200

    Delete watson-sender directory

--- watson-sender/src/ubus.h
+++
@@ -1,9 +0,0 @@
-#include <libubus.h>
-
-typedef struct system_data
-{
-    bool valid;
-    int64_t system_uptime;
-} system_data;
-
-int fetch_system_data(struct ubus_context *ctx, system_data *out_data);
